731bfcb1097e1656de537c4b65ee3cacd81c5180
Yenny Lee <117110034+Yennytime@users.noreply.github.com>
2022-10-31 23:52:06 -0400

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("\\    /\\\n"); // \n이 있기 때문에 세 번 사용해야 됨
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+    // 백슬래시를 출력하기 위해선 두번 연달아 사용(\\)
+    
+    return 0;
+}
