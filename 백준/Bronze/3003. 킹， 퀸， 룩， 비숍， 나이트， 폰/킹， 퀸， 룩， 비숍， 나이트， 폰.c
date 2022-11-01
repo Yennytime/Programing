659747b245cde0b16821963597ebedffd121f967
Yenny Lee <117110034+Yennytime@users.noreply.github.com>
2022-10-31 23:25:40 -0400

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.c

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -6,7 +6,9 @@ int main() {
     int a;
 
     for (int i = 0; i < 6; i++) {
+    // int i = 0, i 가 6보다 작으면 i 값을 4증가
         scanf("%d", &a);
+        // a를 10진수 정수로 변환
         printf("%d ", arr[i] - a);
     }
 
