4ead0992f8315d78f140adff1593039373da55a1
Yenny Lee <117110034+Yennytime@users.noreply.github.com>
2022-11-01 00:07:33 -0400

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+printf("         ,r'\"7\n");
+printf("r`-_   ,'  ,/\n");
+printf(" \\. \". L_r'\n");
+printf("   `~\\/\n");
+printf("      |\n");
+printf("      |\n");
+
+// 문제를 복붙하니까 쉬움;ㅎ
+return 0;
+}
