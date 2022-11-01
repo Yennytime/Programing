7be81bcdc26039e89e682049826dba5d07bdd551
Yenny Lee <117110034+Yennytime@users.noreply.github.com>
2022-10-31 23:59:28 -0400

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    
+    // 따옴표 출력할 때, \" 로 사용
+    
+    return 0;
+}
