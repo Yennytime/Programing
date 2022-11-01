0f2cc5fc840f6394deb57372c2b7dddcc785d081
Yenny Lee <117110034+Yennytime@users.noreply.github.com>
2022-10-31 22:09:02 -0400

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int y;
+        scanf("%d", &y); // 불기년도 = y
+        printf("%d", y - 543);
+        // 2541 - 1998 = 543
+        // 서기년도 변환 출력
+        return 0;
+}
